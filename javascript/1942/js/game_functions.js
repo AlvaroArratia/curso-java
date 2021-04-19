1c5d42adb021726b2c03bcc6dce5a2666c14d0e6
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-04-19 13:32:07 -0400

    bulletCollision

--- javascript/1942/js/game_functions.js
+++ javascript/1942/js/game_functions.js
@@ -12,25 +12,39 @@ var heroPos = {
 
 var enemies = [{
     x: 150,
-    y: 50
+    y: 50,
+    bx: -33,
+    by: -3
 }, {
     x: 250,
-    y: 50
+    y: 50,
+    bx: -123,
+    by: -3
 }, {
     x: 350,
-    y: 50
+    y: 50,
+    bx: -33,
+    by: -3
 }, {
     x: 450,
-    y: 50
+    y: 50,
+    bx: -123,
+    by: -3
 }, {
     x: 550,
-    y: 50
+    y: 50,
+    bx: -33,
+    by: -3
 }, {
     x: 650,
-    y: 50
+    y: 50,
+    bx: -123,
+    by: -3
 }, {
     x: 750,
-    y: 50
+    y: 50,
+    bx: -33,
+    by: -3
 }];
 
 function displayHero() {
@@ -43,10 +57,10 @@ function displayEnemies() {
     for (var i = 0; i < enemies.length; i++) {
         if (i % 2 === 0) {
             output += '<div class="enemy enemy2" style="top:' + enemies[i].y + 'px; left:' + enemies[i].x +
-                'px;"></div>';
+                'px;background-position: ' + enemies[i].bx + 'px ' + enemies[i].by + 'px;"></div>';
         } else {
             output += '<div class="enemy enemy1" style="top:' + enemies[i].y + 'px; left:' + enemies[i].x +
-                'px;"></div>';
+                'px;background-position: ' + enemies[i].bx + 'px ' + enemies[i].by + 'px;"></div>';
         }
     }
     document.getElementById("enemies").innerHTML = output;
@@ -69,8 +83,10 @@ function moveBullets() {
 function enemiesCollision() {
     for (var i = 0; i < enemies.length; i++) {
         if (heroPos.x === enemies[i].x && heroPos.y === enemies[i].y) {
-            heroPoints -= 500;
-            document.getElementById("score").innerText = heroPoints;
+            if (document.getElementsByClassName("enemy")[i].style["background-position"] !== "-109px -30px") {
+                heroPoints -= 500;
+                document.getElementById("score").innerText = heroPoints;
+            }
         }
     }
 }
@@ -79,6 +95,20 @@ function wallCollision() {
     for (var i = 0; i < enemies.length; i++) {
         if (enemies[i].y >= 500) {
             enemies[i].y = 50;
+            if (document.getElementsByClassName("enemy")[i].style["background-position"] === "-109px -30px") {
+                document.getElementById("enemies").removeChild(document.getElementsByClassName("enemy")[i]);
+                enemies.splice(i, 1);
+            }
+        }
+    }
+
+    var bullets = document.getElementsByClassName("bullet");
+    if (bullets.length >= 1) {
+        for (var i = 0; i < bullets.length; i++) {
+            var topValue = parseInt(bullets[i].style["top"].split("px")[0]);
+            if (topValue <= 50) {
+                document.getElementById("bullets").removeChild(document.getElementsByClassName("bullet")[i]);
+            }
         }
     }
 }
@@ -90,15 +120,18 @@ function bulletsCollision() {
         for (var i = 0; i < docEnemies.length; i++) {
             var topValue = parseInt(bullets[j].style["top"].split("px")[0]);
             var leftValue = parseInt(bullets[j].style["left"].split("px")[0]);
-            if (leftValue === enemies[i].x && topValue <= enemies[i].y + 10) {
-                docEnemies[i].style["background-position"] = -109 + "px " + -30 + "px";
+            if (enemies[i].bx !== -109 && enemies[i].by !== -30 && leftValue === enemies[i].x && topValue <= enemies[i].y + 10 && topValue >= enemies[i].y - 10) {
+                document.getElementsByClassName("enemy")[i].style["background-position"] = -109 + "px " + -30 + "px";
+                enemies[i].bx = -109;
+                enemies[i].by = -30;
+                heroPoints += 10;
+                document.getElementById("score").innerText = heroPoints;
             }
         }
     }
 }
 
 function gameLoop() {
-    displayHero();
     moveEnemies();
     moveBullets();
     displayEnemies();
@@ -120,12 +153,13 @@ document.onkeydown = function (e) {
         heroPos.y += 10;
     }
 
+    displayHero();
+    enemiesCollision();
+
     if (e.key === " ") {
         document.getElementById("bullets").innerHTML += '<div class="bullet" style="top:' + (heroPos.y - 10) +
             'px; left:' + heroPos.x + 'px;"></div>';
     }
-    displayHero();
-    enemiesCollision();
 }
 
 gameLoop();
