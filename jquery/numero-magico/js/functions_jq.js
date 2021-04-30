f662e99003e7ad5910d1d9459593a16df638549e
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-04-30 13:19:55 -0400

    some-animations

--- jquery/numero-magico/js/functions_jq.js
+++ jquery/numero-magico/js/functions_jq.js
@@ -1,57 +1,88 @@
 $(document).ready(function () {
     var magicNumber = new MagicNumber();
     $("#restart").hide();
+    $("p").hide();
+    $("#send-num").hide();
+    $("#player-num").hide();
 
     $("#btn-difficulty").click(function () {
         magicNumber.generateRandomNumber();
         magicNumber.setDifficulty = $("#difficulty").val();
         magicNumber.lifesDifficulties();
-        $("#difficulty").attr("disabled", true);
-        $("#btn-difficulty").addClass("disabled");
+        $("#difficulty").fadeOut();
+        $("#btn-difficulty").fadeOut();
+        $("#lifes-info").fadeIn();
+        $("#send-num").fadeIn();
+        $("#player-num").fadeIn();
         $("h2").text("¡Adivina el número magico!");
         $("#lifes-info").text("Intentos: " + magicNumber.getPlayerLifes);
         console.log(magicNumber.getMagicNumber);
     });
 
     $("#send-num").click(function () {
+        $("#num-info span").fadeOut();
         var result = magicNumber.matchNumber($("#player-num").val());
-        var message = "";
+        var message = "",
+            removeClass = "",
+            addClass = "";
         switch (result) {
             case 0:
-                message += "¡Has acertado! :D";
+                message = "¡Has acertado! :D";
+                removeClass = "alert-danger";
+                addClass = "alert-success";
                 break;
             case 1:
-                message += "¡Incorrecto! El número es mayor";
+                message = "¡Incorrecto! El número es mayor";
+                removeClass = "alert-success";
+                addClass = "alert-success";
                 magicNumber.substractLifes();
                 $("#lifes-info").text("Intentos: " + magicNumber.getPlayerLifes);
                 break;
             case 2:
-                message += "¡Incorrecto! El número es menor";
+                message = "¡Incorrecto! El número es menor";
+                removeClass = "alert-success";
+                addClass = "alert-success";
                 magicNumber.substractLifes();
                 $("#lifes-info").text("Intentos: " + magicNumber.getPlayerLifes);
                 break;
             case 3:
-                message += "Has ingresado un número invalido, vuelve a intentarlo";
+                message = "Has ingresado un número invalido, vuelve a intentarlo";
+                removeClass = "alert-success";
+                addClass = "alert-success";
                 break;
             default:
-                message += "Error al comparar los números";
+                message = "Error al comparar los números";
+                removeClass = "alert-success";
+                addClass = "alert-success";
                 break;
         }
         if (magicNumber.youLoose()) {
-            message = "¡Has perdido! :C El número magico era el " + magicNumber.getMagicNumber;
+            $("#num-info span").text("¡Has perdido! :C El número magico era el " + magicNumber.getMagicNumber);
+            $("#num-info span").fadeIn();
             $("#send-num").addClass("disabled");
-            $("#restart").show();
+            $("#restart").fadeIn();
+        } else {
+            $("#num-info span").text(message);
+            $("#num-info").removeClass(removeClass);
+            $("#num-info").addClass(addClass);
+            $("#num-info span").fadeIn();
+            $("#num-info").fadeIn();
         }
-        $("#num-info").text(message);
+
     });
 
     $("#restart").click(function () {
         magicNumber.resetAll();
-        $("#restart").hide();
+        $("#restart").fadeOut();
         $("h2").text("Elige la dificultad");
         $("#lifes-info").text("Intentos: " + magicNumber.getPlayerLifes);
         $("#num-info").text("");
-        $("button").removeClass("disabled");
-        $("#difficulty").attr("disabled", false);
+        $("#difficulty").fadeIn();
+        $("#btn-difficulty").fadeIn();
+        $("#send-num").removeClass("disabled");
+
+        $("p").fadeOut();
+        $("#send-num").fadeOut();
+        $("#player-num").fadeOut();
     });
 });
