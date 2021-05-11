1c1600b5d1c9796bdb289ef374ffdc828f05ce0e
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-05-11 12:44:04 -0400

    swap images

--- jquery/3ninja-gato/js/functions.js
+++ jquery/3ninja-gato/js/functions.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("img").click(function () {
+        var src = $(this).attr("src");
         $(this).attr("src", $(this).attr("data-alt-src"));
+        $(this).attr("data-alt-src", src);
     });
 });
