997f0010928723f724f47fe7e61da767cc9a4806
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-06-01 01:03:51 -0400

    friendships scripts

--- mysql/9friendships.sql
+++ mysql/9friendships.sql
@@ -8,8 +8,10 @@ SELECT CONCAT(users.first_name, ' ', users.last_name) AS user_name, COUNT(*) AS 
 SELECT CONCAT(users.first_name, ' ', users.last_name) AS user_name, COUNT(*) AS friends_num FROM users JOIN friendships ON users.id=friendships.user_id JOIN users AS user2 ON user2.id=friendships.friend_id GROUP BY user_name ORDER BY friends_num DESC LIMIT 1;
 
 # 4.
-#INSERT INTO users (first_name, last_name) VALUES ('Alvaro','Arratia');
-#INSERT INTO friendships (user_id, friend_id) SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Eli Byers' UNION SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Alvaro Arratia';
+INSERT INTO users (first_name, last_name) VALUES ('Alvaro','Arratia');
+INSERT INTO friendships (user_id, friend_id) SELECT (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Eli Byers'), (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Alvaro Arratia');
+INSERT INTO friendships (user_id, friend_id) SELECT (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Kermit The Frog'), (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Alvaro Arratia');
+INSERT INTO friendships (user_id, friend_id) SELECT (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Marky Mark'), (SELECT users.id FROM users WHERE CONCAT(users.first_name, ' ', users.last_name)='Alvaro Arratia');
 
 # 5.
 SELECT CONCAT(users2.first_name,' ', users2.last_name) AS friends_name FROM friendships JOIN users ON users.id=friendships.user_id JOIN users AS users2 ON users2.id=friendships.friend_id WHERE users.first_name='Eli' ORDER BY friends_name ASC;
