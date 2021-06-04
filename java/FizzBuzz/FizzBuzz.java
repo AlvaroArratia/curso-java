2230e8b5c7836ef8a04ccc30d62faa942c7e2986
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-06-04 11:20:57 -0400

    fizz buzz

---
+++ java/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if (number == 2) {
+            return "2";
+        } else if(number == 3) {
+            return "Fizz";
+        } else if(number == 5) {
+            return "Buzz";
+        } else if(number == 15) {
+            return "FizzBuzz";
+        } else {
+            return "Numero no valido";
+        }
+    }
+}
