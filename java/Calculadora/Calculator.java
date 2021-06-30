22dcd7f7d1007919fc7f8e95ddd1ecdd5d8d00d0
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-06-30 16:00:34 -0400

    calculadora 1

---
+++ java/Calculadora/Calculator.java
@@ -0,0 +1,51 @@
+package Calculadora;
+
+public class Calculator {
+
+    private double operandOne;
+    private double operandTwo;
+    private String operation;
+    private double result;
+
+    public Calculator() {
+
+    }
+
+    public double getResult() {
+        performOperation();
+        System.out.println(operandOne + " " + operation + " " + operandTwo);
+        return result;
+    }
+
+    private void performOperation() {
+        if ("+".equals(this.operation)) {
+            this.result = operandOne + operandTwo;
+        } else if ("-".equals(this.operation)) {
+            this.result = operandOne - operandTwo;
+        }
+    }
+
+    public void setOperandOne(double operandOne) {
+        this.operandOne = operandOne;
+    }
+
+    public double getOperandOne() {
+        return operandOne;
+    }
+
+    public void setOperandTwo(double operandTwo) {
+        this.operandTwo = operandTwo;
+    }
+
+    public double getOperandTwo() {
+        return operandTwo;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+}
---
+++ java/Calculadora/CalculatorTest.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator calculadora = new Calculator();
+
+        calculadora.setOperandOne(10.5);
+        calculadora.setOperation("+");
+        calculadora.setOperandTwo(5.2);
+        System.out.println("Resultado = " + calculadora.getResult());
+    }
+}
